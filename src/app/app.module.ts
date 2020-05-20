019d93bc82b2b16a1ea55294e447e16d8cc410c9
bosto <bosto@bosto.net>
2020-05-20 12:28:41 +0200

    Added ContactEditorModule for editing and adding contacts.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactEditorComponent } from './contact-editor/contact-editor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ContactListComponent
+    ContactListComponent,
+    ContactEditorComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/contact-editor/contact-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-editor',
+  templateUrl: './contact-editor.component.html',
+  styleUrls: ['./contact-editor.component.css']
+})
+export class ContactEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
