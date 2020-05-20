9097f8a4659b6a4f96a8310a435f195e6ca48af1
bosto <bosto@bosto.net>
2020-05-20 12:08:49 +0200

    Made use of new error handling service.

--- src/app/contact-manager.service.ts
+++ src/app/contact-manager.service.ts
@@ -4,6 +4,7 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { catchError, map, tap, last } from 'rxjs/operators';
 
 import { Contact } from './contact';
+import { ErrorManagerService } from './error-manager.service';
 
 @Injectable({
   providedIn: 'root'
@@ -20,6 +21,7 @@ export class ContactManagerService {
   public contactUpdated = new Subject();
 
   constructor(
+    private errorManager: ErrorManagerService, 
     private http: HttpClient
   ) { }
 
@@ -27,7 +29,7 @@ export class ContactManagerService {
     console.log('Getting contacs ');
     return this.http.get<Contact[]>(this.url)
     .pipe(
-      catchError(this.handleError<Contact[]>('getContacs', []))
+      catchError(this.errorManager.handleErrorNice<Contact[]>('getContacs', []))
     );
   }
 
@@ -51,14 +53,14 @@ export class ContactManagerService {
     const url = `${this.url}/${id}`;
     return this.http.get<Contact>(url)
     .pipe(
-      catchError(this.handleError<Contact>(`getContact id=${id}`))
+      catchError(this.errorManager.handleErrorNice<Contact>(`getContact id=${id}`))
     );
   }
 
   addContact(contact: Contact): Observable<any> {
     return this.http.post(this.url, contact, this.httpOptions).pipe(
       tap(_ => this.getContactsAsync()),
-      catchError(this.handleError<any>('postContact'))
+      catchError(this.errorManager.handleErrorNice<any>('postContact'))
     );
   }
 
@@ -66,7 +68,7 @@ export class ContactManagerService {
     const url = `${this.url}/${contact.id}`;
     return this.http.put(url, contact, this.httpOptions).pipe(
       tap(_ => this.getContactsAsync()),
-      catchError(this.handleError<any>('updateContact'))
+      catchError(this.errorManager.handleErrorNice<any>('updateContact'))
     );
   }
 
@@ -79,9 +81,9 @@ export class ContactManagerService {
         this.getContactsAsync();
         result = data; }
     ).catch	((error) => {
-      catchError(this.handleError<Contact>('deleteContact'));
-    })
-    if(result != null)
+      catchError(this.errorManager.handleErrorNice<Contact>('deleteContact'));
+    });
+    if (result != null)
     {
       console.log(`result ${result.firstName} came.`)
       return result;
@@ -91,10 +93,10 @@ export class ContactManagerService {
   deleteContact(contact: Contact | number): Observable<Contact> {
     const id = typeof contact === 'number' ? contact : contact.id;
     const url = `${this.url}/${id}`;
-  
+
     return this.http.delete<Contact>(url, this.httpOptions).pipe(
       tap(_ => this.getContactsAsync()),
-      catchError(this.handleError<Contact>('deleteContact'))
+      catchError(this.errorManager.handleErrorNice<Contact>('deleteContact'))
     );
   }
 
@@ -111,7 +113,7 @@ export class ContactManagerService {
 
     console.log(params.toString());
     return this.http.get<Contact[]>(`${searchUrl}`, {params}).pipe(
-      catchError(this.handleError<Contact[]>('searchContacts', []))
+      catchError(this.errorManager.handleErrorNice<Contact[]>('searchContacts', []))
     );
   }
 
@@ -131,29 +133,9 @@ export class ContactManagerService {
       promise.then((data) =>
         this.contactList.next(data)
       ).catch	((error) => {
-        catchError(this.handleError<Contact>('searchContacts'));
+        catchError(this.errorManager.handleErrorNice<Contact>('searchContacts'));
       });
       return promise;
   }
 
- /**
- * Handle Http operation that failed.
- * Let the app continue.
- * @param operation - name of the operation that failed
- * @param result - optional value to return as the observable result
- */
-private handleError<T>(operation = 'operation', result?: T) {
-  return (error: any): Observable<T> => {
-
-    // TODO: send the error to remote logging infrastructure
-    console.error(error); // log to console instead
-
-    // TODO: better job of transforming error for user consumption
-    console.log(`${operation} failed: ${error.message}`);
-    //alert(`${operation} failed: ${error.message}`);
-    
-    // Let the app keep running by returning an empty result.
-    return of(result as T);
-  };
-}
 }
