97f15f0103f9d484889f9dfca0e81a4ccc8d3ff4
bosto <bosto@bosto.net>
2020-05-20 12:10:24 +0200

    Added contact list component for displaying list view.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ContactListComponent
   ],
   imports: [
     BrowserModule,
