cc17d22941cfbdfd0c7463c722673d07f3e67951
bosto <bosto@bosto.net>
2020-05-20 12:48:13 +0200

    Added routes for edit and add actions.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ContactEditorComponent } from './contact-editor/contact-editor.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/contacts', pathMatch: 'full'},
-  { path: 'contacts', component: HomeComponent }
+  { path: 'contacts', component: HomeComponent },
+  { path: 'contacts/:id', component: ContactEditorComponent },
+  { path: 'contacts/add', component: ContactEditorComponent }
 ];
 
 @NgModule({
