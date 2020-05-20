5e9dae75039ca7d54be0d00eaed2ebddd5555588
bosto <bosto@bosto.net>
2020-05-20 14:58:52 +0200

    Implemented SearchComponent.

--- src/app/contact-search/contact-search.component.ts
+++ src/app/contact-search/contact-search.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+
+
+import { Contact } from '../contact';
+import { ContactManagerService } from '../contact-manager.service';
 
 @Component({
   selector: 'app-contact-search',
@@ -6,10 +12,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-search.component.css']
 })
 export class ContactSearchComponent implements OnInit {
+  @Output() contacts: Contact[];
+  
+  private searchFirstNames = new Subject<string>();
+  private searchLastNames = new Subject<string>();
 
-  constructor() { }
+  constructor(private contactManagerService: ContactManagerService) { }
 
   ngOnInit(): void {
   }
+
+  // Push a search term into the observable stream.
+  searchFNames(firstName: string): void {
+    this.searchFirstNames.next(firstName);
+  }
+  searchLNames(lastName: string): void {
+    this.searchLastNames.next(lastName);
+  }
+
+  search(firstName: string, lastName: string, phone: string): void
+  {
+    this.contactManagerService.searchContactsAsync(firstName, lastName, phone);
+  }
+
+  
 
 }
