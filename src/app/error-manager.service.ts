cc12c883482c8de7f1e867ee30f29ca3ca27c304
bosto <bosto@bosto.net>
2020-05-21 20:35:25 +0200

    Made basic http response error handling and alerting.

--- src/app/error-manager.service.ts
+++ src/app/error-manager.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, ErrorHandler } from '@angular/core';
-import { Observable, Subject, of } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
+import { stringify } from 'querystring';
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +16,32 @@ export class ErrorManagerService implements ErrorHandler {
     alert(error);
 }
 
-
+public handleErrorHttpResponse(error: HttpErrorResponse)
+{
+  console.log(`http status: ${error.status}`);
+  let alertMsg;
+  switch(error.status)
+  {
+    case 400:
+      {
+        alertMsg = 'Invalid data!';
+        break;
+      }
+    case 404:
+      {
+        alertMsg = 'Contact not found!';
+        break;
+      }
+    case 409:
+      {
+        alertMsg = 'Duplicate phone number!';
+        break;
+      }
+      default:
+        alertMsg = error.message;
+  }
+  return throwError(alertMsg);
+}
  /**
  * Handle Http operation that failed.
  * Let the app continue.
